72ee3b193ad990f599863c7e8dacdc584197ff30
Lakshman <lakshman.kotipalli@outlook.com>
2018-01-05 01:18:18 +0530

    * getting each img into modal on click

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,6 @@
-function previewImg () {
+function previewImg (src) {
+	console.log(src);
+	getImg(src);
 	document.getElementById('modal').style.display = "block";
 	if(!document.getElementById("modal").classList.contains('show-modal')) {
 		document.getElementById("modal").className += " show-modal";
@@ -12,4 +14,9 @@ function closeModal () {
 		document.getElementById("modal").classList.remove('show-modal');
 		document.getElementById("wrapper").style.opacity = "1";
 	}
+}
+
+function getImg (imgSrc) {
+	var img = document.getElementById('image-view');
+	img.src = imgSrc;
 }
