4f3a4223496faf92496d55b91e02034d70926c81
Lakshman <lakshman.kotipalli@outlook.com>
2018-01-05 00:45:53 +0530

    modal dialog changes

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ function previewImg () {
 	document.getElementById('modal').style.display = "block";
 	if(!document.getElementById("modal").classList.contains('show-modal')) {
 		document.getElementById("modal").className += " show-modal";
+		document.getElementById("wrapper").style.opacity = "0.5";
 	}
 };
 
@@ -9,5 +10,6 @@ function closeModal () {
 	document.getElementById('modal').style.display = "none";
 	if(document.getElementById("modal").classList.contains('show-modal')) {
 		document.getElementById("modal").classList.remove('show-modal');
+		document.getElementById("wrapper").style.opacity = "1";
 	}
 }
